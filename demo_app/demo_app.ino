3e9d75551e7d56206fe60ea8e385eb5fa5d1d4c4
Hoa Bui <hoa.bui@zimplistic.com>
2023-12-01 14:39:50 +0700

    Print blob detected position

--- demo_app/demo_app.ino
+++ demo_app/demo_app.ino
@@ -9,7 +9,7 @@
 #include "esp32cam.h"
 #include "esp32cam/JpegDecoder.h"
 #include "esp32cam/apps/ColorBlobDetector.h"
-#include "esp32cam/http/ColorBlobDetectorHTTP.h"
+#include "ColorBlobDetectorHTTP.h"
 
 using namespace Eloquent::Esp32cam;
 
@@ -18,7 +18,13 @@ JpegDecoder decoder;
 Applications::ColorBlobDetector detector(0, 0, 0); // will configure from the URL
 Http::ColorBlobDetectorHTTP http(cam, decoder, detector);
 
+// http://esp32cam.local/?y=61&cb=37&cr=28
 
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char* ssid = "Hoa";
+const char* password = "6uifMinhHo@f";
 
 void setup() {
     Serial.begin(115200);
@@ -47,7 +53,7 @@ void setup() {
     while (!cam.begin())
         Serial.println(cam.getErrorMessage());
 
-    while (!cam.connect("Zimplistic", "Zimplistic123"))
+    while (!cam.connect(ssid, password))
         Serial.println(cam.getErrorMessage());
 
     while (!http.begin())
@@ -55,9 +61,51 @@ void setup() {
 
     cam.mDNS("esp32cam");
     Serial.println(http.getWelcomeMessage());
+
+
+    /**
+     * Camera setting
+     */
+    sensor_t * s = esp_camera_sensor_get();
+    s->set_brightness(s, 0);     // -2 to 2
+    s->set_contrast(s, 0);       // -2 to 2
+    s->set_saturation(s, 0);     // -2 to 2
+    s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+//    s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+//    s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+//    s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+//    s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+//    s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+//    s->set_ae_level(s, 0);       // -2 to 2
+//    s->set_aec_value(s, 300);    // 0 to 1200
+//    s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+//    s->set_agc_gain(s, 0);       // 0 to 30
+//    s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+//    s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+//    s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+//    s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+//    s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+//    s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+//    s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+//    s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+//    s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
 }
 
 
 void loop() {
     http.handle();
+
+    if (detector.detect(decoder)) {
+        Serial.print("Blob detected from top-left ");
+        Serial.print(detector.blob.top);
+        Serial.print(", ");
+        Serial.print(detector.blob.left);
+        Serial.print(" to bottom-right ");
+        Serial.print(detector.blob.bottom);
+        Serial.print(", ");
+        Serial.println(detector.blob.right);
+        Serial.print("Blob detection run in ");
+        Serial.print(detector.getExecutionTimeInMillis());
+        Serial.println("ms");
+    }
 }
